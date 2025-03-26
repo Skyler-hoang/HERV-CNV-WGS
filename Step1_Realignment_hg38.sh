b53b1ed3fa4dac86fb08d5a783a31b2fc4d63b04
Skyler-hoang <skyler27hoang@gmail.com>
2025-03-26 10:58:49 -0400

    Update Step1_Realignment_hg38.sh

--- Step1_Realignment_hg38.sh
+++ Step1_Realignment_hg38.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=32      # Increase CPU cores (16 instead of 1)
+#SBATCH --mem=128G               # Increase memory (64GB instead of 1GB)
+#SBATCH --time=48:00:00         # Increase time limit if need
 
+
+samtools collate -Oun128 /mnt/pan/SOM_EPBI_SKI/PHONOLOGY/Retrotransposons/hg19_bams/temp_placeholder/CB80_P00050018A.bam | samtools fastq -OT RG,BC - \
+  | /home/hhh38/bwa/bwa mem -pt8 -CH <(samtools view -H /mnt/pan/SOM_EPBI_SKI/PHONOLOGY/Retrotransposons/hg19_bams/temp_placeholder/CB80_P00050018A.bam|grep ^@RG) /mnt/pan/SOM_EPBI_SKI/PHONOLOGY/Retrotransposons/retro_analysis/reference/hg38/hg38.fa - \
+  | samtools sort -@4 -m4g -o /mnt/pan/SOM_EPBI_SKI/PHONOLOGY/Retrotransposons/hg19_bams/temp_placeholder/CB80_P00050018A.hg38.bam -
